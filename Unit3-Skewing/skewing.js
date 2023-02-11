ca4c82d11eb09cc8d0a742932fcde7c233302296
Hagar Barakat <hagarbarakat97@gmail.com>
2023-02-11 15:34:11 +0200

    unit 3 - lesson 3

--- Unit3-Skewing/skewing.js
+++ Unit3-Skewing/skewing.js
@@ -1,37 +1,47 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   let x,y, w, h;
-  let rx, ry,  angle; 
+  
+  const degrees = -30;
+  const num = 20; 
+  const rects = [];
 
+  for(let i = 0; i < num; i++ ){
+    x = random.range(0, width);
+    y = random.range(0, height); 
+    w = random.range(200, 600); 
+    h = random.range(40, 200); 
+
+    rects.push({x, y, w, h});
+  }
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+   rects.forEach(rect => {
 
-    x = width * 0.5;
-    y = height * 0.5; 
-    w = width * 0.6; 
-    h = height * 0.1; 
+      const {x, y, w, h} = rect;
+      context.save();
+      context.translate(x, y);
 
-    context.save();
-    context.translate(x, y);
+      context.strokeStyle = 'blue'; 
 
-    context.strokeStyle = 'blue'; 
+      drawSkewedRect({context, w, h, degrees});
+      context.stroke();
 
-    drawSkewedRect({context});
-    context.stroke();
-
-    context.restore();
+      context.restore();
+    });
   };
 };
 
 const drawSkewedRect = ({context, w = 600, h = 200, degrees = -45}) => {
-  const angle = math.degToRad(15);
+  const angle = math.degToRad(degrees);
   const rx = Math.cos(angle) * w; 
   const ry = Math.sin(angle) * w;
   
