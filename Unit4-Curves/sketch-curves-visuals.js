bc63f1aa18f82d73f66d03406199bb8ee0ba3083
Hagar Barakat <hagarbarakat97@gmail.com>
2023-02-13 00:25:34 +0200

    unit 4 - lesson 6

--- Unit4-Curves/sketch-curves-visuals.js
+++ Unit4-Curves/sketch-curves-visuals.js
@@ -4,7 +4,8 @@ const math = require('canvas-sketch-util/math');
 const colormap = require('colormap');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ], 
+  animate: true,
 };
 
 const sketch = ({ width, height }) => {
@@ -43,8 +44,8 @@ const sketch = ({ width, height }) => {
     y = Math.floor(i / cols) * ch;
 
     n = random.noise2D(x, y, frequency, amplitude);
-    x += n; 
-    y += n;
+    // x += n; 
+    // y += n;
 
     lineWidth = math.mapRange(n, -amplitude, amplitude, 0, 5);
     color = colors[Math.floor(math.mapRange(n, -amplitude, amplitude, 0, amplitude))];
@@ -52,7 +53,7 @@ const sketch = ({ width, height }) => {
     points.push(new Point({ x, y , lineWidth, color}));
   }
 
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
@@ -61,6 +62,14 @@ const sketch = ({ width, height }) => {
     context.translate(cw * 0.5, ch * 0.5);
     context.strokeStyle = 'red'; 
     context.lineWidth = 4; 
+
+    //update position
+    points.forEach(point => {
+
+      n = random.noise2D(point.ix + frame * 2, point.iy, frequency, amplitude);
+      point.x = point.ix + n; 
+      point.y = point.iy + n;
+    });
 
     let lastx, lasty;
 
@@ -111,6 +120,9 @@ class Point{
       this.y = y; 
       this.lineWidth = lineWidth;
       this.color = color;
+
+      this.ix = x;
+      this.iy = y;
   }
 
   draw(context){
